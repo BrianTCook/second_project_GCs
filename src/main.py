d22eb0b2a39277b65a0eb4d0d3c6f22ae0db241c
Brian <btcook@umich.edu>
2019-11-27 15:44:53 +0100

    this one will take a while

--- src/main.py
+++ src/main.py
@@ -335,11 +335,11 @@ def main(Rgal, Mgal, alpha, gravity_solvers, Nclusters, Nstars, W0, M,
 if __name__ == '__main__':
 
     Mgal, Rgal, alpha = 1.6e10|units.MSun, 1000.|units.parsec, 1.2
-    Nclusters = 1
-    Nstars, W0cluster, Mcluster, Rcluster = 50, 1.5, 100.|units.MSun, 1.|units.parsec
+    Nclusters = 40
+    Nstars, W0cluster, Mcluster, Rcluster = 100, 1.5, 100.|units.MSun, 1.|units.parsec
     Rinit = 1000.|units.parsec
     parameters = [('epsilon_squared', 0.01|(units.parsec**2))]
-    t_end, dt = 40.|units.Myr, 1.|units.Myr
+    t_end, dt = 100.|units.Myr, 1.|units.Myr
 
     gravity_solvers = [ 'Brute' ] #'Nemesis'
 

f8096ca7ea4a49d83a3a05cf9ada6120c84c3254
Brian Cook <btcook@umich.edu>
2020-01-22 16:26:19 +0100

    have a bunch of png files

---
+++ src/phase_space_mapping.py
@@ -0,0 +1,117 @@
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+
+gravity_solver_str = 'Brute'
+Nclusters = 10
+
+data_dir = '/Users/BrianTCook/Desktop/Thesis/second_project_GCs/data/'
+filename_time = 'time_data_Nclusters=%i_%s.npy'%(Nclusters, gravity_solver_str)
+filename_phase = 'sixD_data_Nclusters=%i_%s.npy'%(Nclusters, gravity_solver_str)    
+
+sim_times = np.load(data_dir+filename_time)
+phase_space_data = np.load(data_dir+filename_phase)
+ntimes, ndim, nparticles = phase_space_data.shape
+print(ntimes, ndim, nparticles)
+
+for i, t in enumerate(sim_times):
+    
+    w_all = pd.DataFrame(np.rot90(phase_space_data[i,:,:]), 
+                         columns=['x', 'y', 'z', 'vx', 'vy', 'vz'])
+    
+    x,y,z = w_all['x'].tolist(), w_all['y'].tolist(), w_all['z'].tolist()
+    vx,vy,vz = w_all['vx'].tolist(), w_all['vy'].tolist(), w_all['vz'].tolist()
+    
+    fig, axs = plt.subplots(5, 5)
+    
+    #first column
+    
+    axs[0, 0].scatter(x, y, s=2)
+    axs[0, 0].set(xlabel='x', ylabel='y')
+    axs[0, 0].set_xlim(-1000, 1000)
+    axs[0, 0].set_ylim(-1000, 1000)
+    
+    axs[1, 0].scatter(x, z, s=2)
+    axs[1, 0].set(xlabel='x', ylabel='z')
+    axs[1, 0].set_xlim(-1000, 1000)
+    axs[1, 0].set_ylim(-1000, 1000)
+    
+    axs[2, 0].scatter(x, vx,s=2)
+    axs[2, 0].set(xlabel='x', ylabel='vx')
+    axs[2, 0].set_xlim(-1000, 1000)
+    axs[2, 0].set_ylim(-400, 400)
+    
+    axs[3, 0].scatter(x, vy, s=2)
+    axs[3, 0].set(xlabel='z', ylabel='vy')
+    axs[3, 0].set_xlim(-1000, 1000)
+    axs[3, 0].set_ylim(-400, 400)
+    
+    axs[4, 0].scatter(x, vz, s=2)
+    axs[4, 0].set(xlabel='x', ylabel='vz')
+    axs[4, 0].set_xlim(-1000, 1000)
+    axs[4, 0].set_ylim(-400, 400)
+    
+    #second column
+    
+    axs[1, 1].scatter(y, z, s=2)
+    axs[1, 1].set(xlabel='y', ylabel='z')
+    axs[1, 1].set_xlim(-1000, 1000)
+    axs[1, 1].set_ylim(-1000, 1000)
+    
+    axs[2, 1].scatter(y, vx, s=2)
+    axs[2, 1].set(xlabel='y', ylabel='vx')
+    axs[2, 1].set_xlim(-1000, 1000)
+    axs[2, 1].set_ylim(-400, 400)
+    
+    axs[3, 1].scatter(y, vy, s=2)
+    axs[3, 1].set(xlabel='y', ylabel='vy')
+    axs[3, 1].set_xlim(-1000, 1000)
+    axs[3, 1].set_ylim(-400, 400)
+
+    axs[4, 1].scatter(vy, vz, s=2)
+    axs[4, 1].set(xlabel='y', ylabel='vz')
+    axs[4, 1].set_xlim(-1000, 1000)
+    axs[4, 1].set_ylim(-400, 400)
+    
+    #third column
+    
+    axs[2, 2].scatter(z, vx, s=2)
+    axs[2, 2].set(xlabel='z', ylabel='vx')
+    axs[2, 2].set_xlim(-1000, 1000)
+    axs[2, 2].set_ylim(-400, 400)
+    
+    axs[3, 2].scatter(z, vy, s=2)
+    axs[3, 2].set(xlabel='z', ylabel='vy')
+    axs[3, 2].set_xlim(-1000, 1000)
+    axs[3, 2].set_ylim(-400, 400)
+
+    axs[4, 2].scatter(z, vz, s=2)
+    axs[4, 2].set(xlabel='z', ylabel='vz')
+    axs[4, 2].set_xlim(-1000, 1000)
+    axs[4, 2].set_ylim(-400, 400)
+    
+    #fourth column
+    
+    axs[3, 3].scatter(vx, vy, s=2)
+    axs[3, 3].set(xlabel='vx', ylabel='vy')
+    axs[3, 3].set_xlim(-400, 400)
+    axs[3, 3].set_ylim(-400, 400)
+
+    axs[4, 3].scatter(vx, vz, s=2)
+    axs[4, 3].set(xlabel='vx', ylabel='vz')
+    axs[4, 3].set_xlim(-400, 400)
+    axs[4, 3].set_ylim(-400, 400)
+    
+    #fifth column
+    
+    axs[4, 4].scatter(vy, vz, s=2)
+    axs[4, 4].set(xlabel='vy', ylabel='vz')
+    axs[4, 4].set_xlim(-400, 400)
+    axs[4, 4].set_ylim(-400, 400)
+    
+    # Hide x labels and tick labels for top plots and y ticks for right plots.
+    for ax in axs.flat:
+        ax.label_outer()
+    
+    plt.savefig('phase_space_map_frame=%s.png'%(str(i).rjust(4, '0')))
+    plt.close()
--- src/test_superfreq.py
+++ src/test_superfreq.py
@@ -3,39 +3,43 @@ import matplotlib.pyplot as plt
 from superfreq import SuperFreq
 
 gravity_solver_str = 'Brute'
-Nclusters = 1
-
-data_dir = '/Users/BrianTCook/Desktop/Thesis/second_project_GCs/data/'
-filename_time = 'time_data_Nclusters=%i_%s.npy'%(Nclusters, gravity_solver_str)
-filename_phase = 'sixD_data_Nclusters=%i_%s.npy'%(Nclusters, gravity_solver_str)
-
-sim_times = np.load(data_dir+filename_time)
-phase_space_data = np.load(data_dir+filename_phase)
-ntimes, ndim, nparticles = phase_space_data.shape
-print(ntimes, ndim, nparticles)
-
-#fundamental frequency stuff
-freqs_all = []
-for k in range(nparticles):
-    t, w = sim_times, phase_space_data[:, :, k]
-    sf = SuperFreq(t)
-    fs = [ (w[:,i] * 1j * w[:,i+ndim//2]) for i in range(ndim//2) ]
-    superfreq_result_object = sf.find_fundamental_frequencies(fs)
-    freqs_all.append( superfreq_result_object.fund_freqs )
-
-print(freqs_all)
-freqs_all = [j for i in freqs_all for j in i]
-freqs_all = np.sort(freqs_all)
-print(freqs_all)
-
-X_cdf = np.linspace(min(freqs_all), max(freqs_all), len(freqs_all))
-dX_cdf = X_cdf[1] - X_cdf[0]
-
-Y_cdf = freqs_all / ( (freqs_all*dX_cdf).sum() )
-cumulated_Y = np.cumsum(Y_cdf * dX_cdf)
+Nclusters_list = [1, 10]
 
 plt.figure()
-plt.hist(freqs_all, bins=100)
-#plt.plot(X_cdf, cumulated_Y, 'k', label='CDF')
-plt.xlabel('Frequency (units?)', fontsize=12)
-plt.savefig('fundamental_freqs_Nclusters=%i_%s.png'%(Nclusters, gravity_solver_str))
+
+for Nclusters in Nclusters_list:
+
+    data_dir = '/Users/BrianTCook/Desktop/Thesis/second_project_GCs/data/'
+    filename_time = 'time_data_Nclusters=%i_%s.npy'%(Nclusters, gravity_solver_str)
+    filename_phase = 'sixD_data_Nclusters=%i_%s.npy'%(Nclusters, gravity_solver_str)
+    
+    sim_times = np.load(data_dir+filename_time)
+    phase_space_data = np.load(data_dir+filename_phase)
+    ntimes, ndim, nparticles = phase_space_data.shape
+    print(ntimes, ndim, nparticles)
+    
+    #fundamental frequency stuff
+    freqs_all = []
+    for k in range(nparticles):
+        t, w = sim_times, phase_space_data[:, :, k]
+        sf = SuperFreq(t)
+        fs = [ (w[:,i] * 1j * w[:,i+ndim//2]) for i in range(ndim//2) ]
+        superfreq_result_object = sf.find_fundamental_frequencies(fs)
+        freqs_all.append( superfreq_result_object.fund_freqs )
+    
+    freqs_all = [j for i in freqs_all for j in i] #concatenate the list
+    freqs_all = np.sort(freqs_all)
+    
+    yvals, xvals = np.histogram(freqs_all)
+    xvals_CDF = [ 0.5*(xvals[i]+xvals[i+1]) for i in range(len(xvals)-1) ] 
+    
+    ysum = sum(yvals)
+    yvals = [ y/ysum for y in yvals ] #normalization
+    
+    yvals_CDF = np.cumsum(yvals)
+    
+    plt.plot(xvals_CDF, yvals_CDF, label='CDF, Nclusters = %i'%(Nclusters))
+    
+plt.xlabel('Frequency (action-angle variable units)', fontsize=12)
+plt.legend(loc='best')
+plt.savefig('fundamental_freqs_CDF_%s.png'%(gravity_solver_str))

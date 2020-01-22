057eb75ae25ace1a49f545994071acf1f6232ab7
Brian Cook <btcook@umich.edu>
2020-01-22 11:03:42 +0100

    need to save time asan array too

--- src/test_superfreq.py
+++ src/test_superfreq.py
@@ -1,16 +1,24 @@
-import numpy as np #known to work
-from superfreq import SuperFreq #not so much
-print('hello world!')
+import numpy as np 
+import matplotlib.pyplot as plt
+from superfreq import SuperFreq
+
+gravity_solver_str = 'Brute'
+data_dir = '/Users/BrianTCook/Desktop/Thesis/second_project_GCs/data/'
+filename_time = 'time_data_Nclusters=1_%s.npy'%(gravity_solver_str)
+filename_phase = 'sixD_data_Nclusters=1_%s.npy'%(gravity_solver_str)
+
+phase_space_data = np.load(data_dir+filename)
+ntimes, ndim, nparticles = phase_space_data.shape
+print(ntimes, ndim, nparticles)
 
 #fundamental frequency stuff
 freqs_all = []
-for k in range(len(gravity.particles)):
-	t, w = sim_times, phase_space_data[:, :, k]
-	ntimes, ndim = w.shape
-	sf = SuperFreq(t)
-	fs = [ (w[:,i] * 1j * w[:,i+ndim//2]) for i in range(ndim//2) ]
-	freqs, tbl, ix = sf.find_fundamental_frequencies(fs)
-	freqs_all.append(freqs)
+for k in range(nparticles):
+    t, w = sim_times, phase_space_data[:, :, k]
+    sf = SuperFreq(t)
+    fs = [ (w[:,i] * 1j * w[:,i+ndim//2]) for i in range(ndim//2) ]
+    freqs, tbl, ix = sf.find_fundamental_frequencies(fs)
+    freqs_all.append(freqs)
 
 freqs_all = np.sort(freqs_all)
 

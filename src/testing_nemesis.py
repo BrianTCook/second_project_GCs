6a7efcde444a04e61e8f63e68a7fa6c0ffe1b8a6
Brian Cook <btcook@umich.edu>
2020-02-04 16:36:30 +0100

    cheap move but maybe it will work

--- src/testing_nemesis.py
+++ src/testing_nemesis.py
@@ -78,7 +78,7 @@ def make_king_model_cluster(Rcoord, Zcoord, phicoord, vr_init, vphi_init, vz_ini
     #vphi = R \dot{\phi}? assuming yes for now
     vx_init = (vr_init*np.cos(phicoord) - vphi_init*np.sin(phicoord)) | units.kms
     vy_init = (vr_init*np.sin(phicoord) + vphi_init*np.cos(phicoord)) | units.kms
-    vz_init = vz_init | units.kms
+    vz_init = vz_init #| units.kms
     
     pos_vec, vel_vec = (x_init, y_init, z_init), (vx_init, vy_init, vz_init)
     

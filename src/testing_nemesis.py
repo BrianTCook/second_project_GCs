3835c4120d4b6caf3f9daa3165fbdf471ac192b3
Brian Cook <btcook@umich.edu>
2020-02-07 14:55:29 +0100

    more print statements for nemesis committing

--- src/testing_nemesis.py
+++ src/testing_nemesis.py
@@ -375,13 +375,12 @@ def gravity_code_setup(orbiter_name, code_name, galaxy_code, Rcoord, Zcoord, phi
         nemesis.particles.add_particles(parts)
         nemesis.commit_particles()
 
+        print('nemesis.particles.compound_particles(): ', nemesis.particles.compount_particles())
         print('nemesis.subcodes are: ', nemesis.subcodes)
 
         #gravity = bridge.Bridge(use_threading=False)
         #gravity.add_system(nemesis, (galaxy_code,))
         #gravity.timestep = dt_bridge
-        #gravity.commit_parameters()
-        #gravity.commit_particles()
 
     return orbiter_bodies, nemesis #gravity
 

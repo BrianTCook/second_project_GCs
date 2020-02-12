4c4d109d85109fee981ce0f535e036ad264cf5d9
Brian Cook <btcook@umich.edu>
2020-02-12 14:24:39 +0100

    spring cleaning

--- src/galpy_ahh.py
+++
@@ -1,3 +0,0 @@
-from galpy.util import bovy_conversion
-
-print(bovy_conversion.velocity_in_kpcGyr(220., 8.))
--- src/particles_amuse.py
+++
@@ -1,7 +0,0 @@
-from amuse.lab import *
-
-ahh = Particles(1)
-ahh.mass = 1.|units.kg
-ahh.x = 1.|units.kpc
-
-print(ahh)

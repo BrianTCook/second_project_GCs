d123646d236354b8f467c44a82917f2daa4997aa
Brian Cook <btcook@umich.edu>
2020-02-14 13:43:32 +0100

    cleaned up distribution plots

--- src/plot_cluster_distributions.py
+++ src/plot_cluster_distributions.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
@@ -7,10 +9,20 @@ from coordinate_generator import radial_coords, zed_coords
 r_OC, r_YMC, r_GC = radial_coords()
 z_OC, z_YMC, z_GC = zed_coords()
 
+N_OC, N_YMC, N_GC = len(r_OC), len(r_YMC), len(r_GC)
+
+y_r_OC, x_r_OC = np.histogram(r_OC, bins=50, density=True)
+y_r_YMC, x_r_YMC = np.histogram(r_YMC, bins=50, density=True)
+y_r_GC, x_r_GC = np.histogram(r_GC, bins=50, density=True)
+
+y_z_OC, x_z_OC = np.histogram(z_OC, bins=50, density=True)
+y_z_OC, x_z_YMC = np.histogram(z_YMC, bins=50, density=True)
+y_z_OC, x_z_GC = np.histogram(z_GC, bins=50, density=True)
+
 plt.figure()
-plt.hist(r_OC, density=True, cumulative=False, histtype='step', label='Open Clusters')
-plt.hist(r_YMC, density=True, cumulative=False, histtype='step', label='Young Massive Clusters')
-plt.hist(r_GC, density=True, cumulative=False, histtype='step', label='Globular Clusters')
+plt.plot( [ 0.5*(x_r_OC[i]+x_r_OC[i+1]) for i in range(N_OC-1) ], y_r_OC, label='Open Clusters')
+plt.plot( [ 0.5*(x_r_YMC[i]+x_r_YMC[i+1]) for i in range(N_YMC-1) ], y_r_YMC, label='Young Massive Clusters')
+plt.plot( [ 0.5*(x_r_GC[i]+x_r_GC[i+1]) for i in range(N_GC-1) ], y_r_GC, label='Globular Clusters')
 plt.xlabel(r'$r$ (kpc)', fontsize=16)
 plt.ylabel(r'Probability Density', fontsize=16)
 plt.legend(loc='upper right')
@@ -18,9 +30,9 @@ plt.savefig('radial_coord_distribution.pdf')
 plt.close()
 
 plt.figure()
-plt.hist(z_OC, density=True, cumulative=False, histtype='step', label='Open Clusters')
-plt.hist(z_YMC, density=True, cumulative=False, histtype='step', label='Young Massive Clusters')
-plt.hist(z_GC, density=True, cumulative=False, histtype='step', label='Globular Clusters')
+plt.plot( [ 0.5*(x_z_OC[i]+x_z_OC[i+1]) for i in range(N_OC-1) ], y_z_OC, label='Open Clusters')
+plt.plot( [ 0.5*(x_z_YMC[i]+x_z_YMC[i+1]) for i in range(N_YMC-1) ], y_z_YMC, label='Young Massive Clusters')
+plt.plot( [ 0.5*(x_z_GC[i]+x_z_GC[i+1]) for i in range(N_GC-1) ], y_z_GC, label='Globular Clusters')
 plt.xlabel(r'$z$ (kpc)', fontsize=16)
 plt.ylabel(r'Probability Density', fontsize=16)
 plt.legend(loc='upper right')

0d4da3b3c026a6f3b100f89018badcfd3eef7f82
Brian <btcook@umich.edu>
2020-02-18 17:49:26 +0100

    have cluster mass distribution

--- src/cluster_mass_distribution.py
+++ src/cluster_mass_distribution.py
@@ -3,18 +3,22 @@
 """
 Created on Tue Feb 18 17:40:05 2020
 
+creates a list of 10000 AMUSE masses
+[100, 1000000] MSuns
+sampled from appropriate power law
+saves them as a .txt file
+
 @author: BrianTCook
 """
 
 from amuse.lab import *
-from amuse.ic.kingmodel import new_king_model
-from amuse.community.mercury.interface import Mercury
+import numpy as np
 
 if __name__ in '__main__':
     
     mZams = new_powerlaw_mass_distribution(10000,
-                                           0.1|units.MSun,
-                                           100.|units.Msun,
+                                           float(int(1e2))|units.MSun,
+                                           float(int(1e6))|units.MSun,
                                            alpha=-2.)
     
-    np.savetxt('cluster_masses_for_sampling.txt', mZams)
+    np.savetxt('cluster_masses_for_sampling.txt', mZams.value_in(units.MSun))

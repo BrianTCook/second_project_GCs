e4929093f103378efa01d66f43f2352831710590
Brian Cook <btcook@umich.edu>
2020-02-24 00:04:21 +0100

    uncomment simulation line

--- src/testing_nemesis.py
+++ src/testing_nemesis.py
@@ -527,13 +527,13 @@ if __name__ in '__main__':
             print(orbiter_name, code_name)
             print('\\\\\\\\\\\\\\\\\\\\\\\\')
             
-            #simulation(orbiter_name, code_name, potential, Mgalaxy, Rgalaxy, 
-                       #sepBinary, rvals, phivals, zvals, vrvals, vphivals, vzvals, 
-                       #masses, tend, dt)
+            simulation(orbiter_name, code_name, potential, Mgalaxy, Rgalaxy, 
+                       sepBinary, rvals, phivals, zvals, vrvals, vphivals, vzvals, 
+                       masses, tend, dt)
             
             print('current time: %.03f minutes'%((time.time()-t0)/60.))
             
-            #maps(orbiter_name, code_name)
+            maps(orbiter_name, code_name)
             
             print('current time: %.03f minutes'%((time.time()-t0)/60.))
             

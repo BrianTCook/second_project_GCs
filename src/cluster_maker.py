8199d9576d76e709de3756d6612f300f7a24154f
Brian Cook <btcook@umich.edu>
2020-02-23 20:32:32 +0100

    unit problem

--- src/cluster_maker.py
+++ src/cluster_maker.py
@@ -120,7 +120,7 @@ def star_cluster(rvals, phivals, zvals, vrvals, vphivals, vzvals, masses, index,
     vr_init, vphi_init, vz_init = vrvals[index], vphivals[index], vzvals[index]
     
     star_masses = np.loadtxt('/home/brian/Desktop/second_project_gcs/data/star_masses_index=%i.txt'%index)
-    Mcluster = np.sum( star_masses.value_in(units.MSun ) )
+    Mcluster = np.sum( star_masses )
     
     star_masses, Mcluster = star_masses|units.MSun, Mcluster|units.MSun
     

944738867bd290ba8d345d32edc833349abde826
Brian <btcook@umich.edu>
2020-02-26 17:51:40 +0100

    need to run for longer and with more cluster to ensure dE is acting properly

--- src/FebFive.sh
+++ src/FebFive.sh
@@ -3,11 +3,13 @@
 echo 'run script'
 python3 testing_nemesis.py
 
+'''
 echo 'create gifs'
 convert -delay 10 'snapshot_Nbody_SingleStar_*.png' -loop 0 Nbody_singlestar.gif
 convert -delay 10 'snapshot_tree_SingleStar_*.png' -loop 0 tree_singlestar.gif
 convert -delay 10 'snapshot_Nbody_SingleCluster_*.png' -loop 0 Nbody_singlecluster.gif
 convert -delay 10 'snapshot_tree_SingleCluster_*.png' -loop 0 tree_singlecluster.gif
+'''
 
 rm -rf snapshot_*.png
 mv *.txt ../data
@@ -16,3 +18,4 @@ mv *.csv ../data
 mv *.pdf ../figures
 mv *.png ../figures
 mv *.gif ../figures
+

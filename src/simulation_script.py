9746ad4bdb1aa9578987c6b2f6b28642879aa712
Brian Cook <btcook@umich.edu>
2020-02-27 11:48:17 +0100

    consistent numpy indexing

--- src/simulation_script.py
+++ src/simulation_script.py
@@ -66,7 +66,7 @@ def simulation(orbiter_name, code_name, potential, Mgalaxy, Rgalaxy, sepBinary,
     
     #for 3D numpy array storage
     all_data = np.zeros((len(sim_times), Ntotal, 7))
-    COM_data = np.zeros((len(sim_times), 2, Norbiters))
+    COM_data = np.zeros((len(sim_times), Norbiters, 2))
     
     cluster_pop_flag = 0
     
@@ -113,8 +113,8 @@ def simulation(orbiter_name, code_name, potential, Mgalaxy, Rgalaxy, sepBinary,
             x_COM = np.sum( [ body_masses[i]*x[i]/cluster_total_mass for i in range(starting_index, ending_index) ] ) #in kpc
             y_COM = np.sum( [ body_masses[i]*y[i]/cluster_total_mass for i in range(starting_index, ending_index) ] ) #in kpc
         
-            COM_data[j, 0, k] = x_COM
-            COM_data[j, 1, k] = y_COM
+            COM_data[j, k, 0] = x_COM
+            COM_data[j, k, 1] = y_COM
         
         
         gravity.evolve_model(t)

1106f5e18fb047f813c74de33f9e2cabb80b466e
Brian Cook <btcook@umich.edu>
2020-02-28 12:50:06 +0100

    import gzip

--- src/simulation_script.py
+++ src/simulation_script.py
@@ -15,6 +15,7 @@ from galpy.util import bovy_conversion
 
 from gravity_code import gravity_code_setup
 
+import gzip
 import numpy as np
 import pandas as pd
 import time
@@ -125,10 +126,10 @@ def simulation(orbiter_name, code_name, potential, Mgalaxy, Rgalaxy, sepBinary,
     gravity.stop()
     
     #things that are not easily extracted from write_set_to_file
-    np.save('all_data_%s_%s_Norbiter_%s.npy.gz'%(code_name, orbiter_name, str(Norbiters)), all_data,
-            allow_pickle=True)
-    np.save('COM_data_%s_%s_Norbiters_%s.npy.gz'%(code_name, orbiter_name, str(Norbiters)), COM_data,
-            allow_pickle=True)
+    f_all = gzip.GzipFile('all_data_%s_%s_Norbiter_%s.npy.gz'%(code_name, orbiter_name, str(Norbiters)), 'w')
+    f_COM = gzip.GzipFile('COM_data_%s_%s_Norbiters_%s.npy.gz'%(code_name, orbiter_name, str(Norbiters)), 'w')
+    np.save(file=f_all, arr=all_data, allow_pickle=True)
+    np.save(file=f_COM, arr=COM_data, allow_pickle=True)
     np.savetxt(code_name + '_' + orbiter_name + '_colors_Norbiters_' + str(Norbiters) + '.txt', cluster_colors)
     np.savetxt(code_name + '_' + orbiter_name + '_dE_Norbiters_' + str(Norbiters) + '.txt', delta_energies)
     np.savetxt(code_name + '_' + orbiter_name + '_clock_times.txt', clock_times)

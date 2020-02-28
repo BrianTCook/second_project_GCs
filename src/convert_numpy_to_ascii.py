2e58f029171f3d4779186bd02ff6e68695846263
Brian Cook <btcook@umich.edu>
2020-02-28 12:25:53 +0100

    need to think about large .npy files

--- src/convert_numpy_to_ascii.py
+++ src/convert_numpy_to_ascii.py
@@ -1,13 +1,20 @@
 import numpy as np
 
-code_name, orbiter_name, Norbiters = 'tree', 'SingleCluster', 8
+code_names = [ 'tree', 'Nbody' ]
+orbiter_names = [ 'SingleStar', 'SingleCluster' ]
+Norbiters_list = [ 1 ]
+
 data_directory = '/home/brian/Desktop/second_project_gcs/data/'
 
-all_data = np.load(data_directory+'all_data_%s_%s_Norbiter_%s.npy'%(code_name, orbiter_name, str(Norbiters)))
-N_timesteps = len(all_data[:,0,0])
+for code_name in code_names:
+    for orbiter_name in orbiter_names:
+        for Norbiters in Norbiters_list:
 
-for i in range(N_timesteps):
-    
-    data_to_keep = all_data[i, :, 0:] #gets rid of mass
-    np.savetxt('for_enbid_%s_%s_frame_%s_Norbiters_%s.txt'%(code_name, orbiter_name,
-                                                            str(i).rjust(5, '0'), str(Norbiters)))
+            all_data = np.load(data_directory+'all_data_%s_%s_Norbiter_%s.npy'%(code_name, orbiter_name, str(Norbiters)))
+            N_timesteps = len(all_data[:,0,0])
+            
+            for i in range(N_timesteps):
+                
+                data_to_keep = all_data[i, :, 0:] #gets rid of mass
+                np.savetxt('for_enbid_%s_%s_frame_%s_Norbiters_%s.txt'%(code_name, orbiter_name,
+                                                                        str(i).rjust(5, '0'), str(Norbiters)))

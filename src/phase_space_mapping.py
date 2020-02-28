cf53a491c29a65ada10f2a9bcfb4ee9dbb09cdf0
Brian Cook <btcook@umich.edu>
2020-02-28 15:06:36 +0100

    includes colors

--- src/phase_space_mapping.py
+++ src/phase_space_mapping.py
@@ -11,6 +11,7 @@ def maps(code_name, orbiter_name, Norbiters):
     
     sim_times = np.loadtxt('times_in_Myr_%s_%s_Norbiters_%i.txt'%(code_name, orbiter_name, Norbiters))    
     filename_phase = 'all_data_%s_%s_Norbiters_%s.npy.gz'%(code_name, orbiter_name, str(Norbiters))    
+    colors = np.loadtxt(code_name + '_' + orbiter_name + '_colors_Norbiters_' + str(Norbiters) + '.txt')
     
     f_all = gzip.GzipFile('all_data_%s_%s_Norbiters_%s.npy.gz'%(code_name, orbiter_name, str(Norbiters)), 'r')
     phase_space_data = np.load(f_all)
@@ -31,85 +32,85 @@ def maps(code_name, orbiter_name, Norbiters):
             
             #first column
             
-            axs[0, 0].scatter(x, y, s=2, c='k')
+            axs[0, 0].scatter(x, y, s=2, c=colors)
             axs[0, 0].set(xlabel='x', ylabel='y')
             axs[0, 0].set_xlim(-2, 2)
             axs[0, 0].set_ylim(-2, 2)
             
-            axs[1, 0].scatter(x, z, s=2, c='k')
+            axs[1, 0].scatter(x, z, s=2, c=colors)
             axs[1, 0].set(xlabel='x', ylabel='z')
             axs[1, 0].set_xlim(-2, 2)
             axs[1, 0].set_ylim(-2, 2)
             
-            axs[2, 0].scatter(x, vx,s=2, c='k')
+            axs[2, 0].scatter(x, vx,s=2, c=colors)
             axs[2, 0].set(xlabel='x', ylabel='vx')
             axs[2, 0].set_xlim(-2, 2)
             axs[2, 0].set_ylim(-400, 400)
             
-            axs[3, 0].scatter(x, vy, s=2, c='k')
+            axs[3, 0].scatter(x, vy, s=2, c=colors)
             axs[3, 0].set(xlabel='z', ylabel='vy')
             axs[3, 0].set_xlim(-2, 2)
             axs[3, 0].set_ylim(-400, 400)
             
-            axs[4, 0].scatter(x, vz, s=2, c='k')
+            axs[4, 0].scatter(x, vz, s=2, c=colors)
             axs[4, 0].set(xlabel='x', ylabel='vz')
             axs[4, 0].set_xlim(-2, 2)
             axs[4, 0].set_ylim(-400, 400)
             
             #second column
             
-            axs[1, 1].scatter(y, z, s=2, c='k')
+            axs[1, 1].scatter(y, z, s=2, c=colors)
             axs[1, 1].set(xlabel='y', ylabel='z')
             axs[1, 1].set_xlim(-2, 2)
             axs[1, 1].set_ylim(-2, 2)
             
-            axs[2, 1].scatter(y, vx, s=2, c='k')
+            axs[2, 1].scatter(y, vx, s=2, c=colors)
             axs[2, 1].set(xlabel='y', ylabel='vx')
             axs[2, 1].set_xlim(-2, 2)
             axs[2, 1].set_ylim(-400, 400)
             
-            axs[3, 1].scatter(y, vy, s=2, c='k')
+            axs[3, 1].scatter(y, vy, s=2, c=colors)
             axs[3, 1].set(xlabel='y', ylabel='vy')
             axs[3, 1].set_xlim(-2, 2)
             axs[3, 1].set_ylim(-400, 400)
             
-            axs[4, 1].scatter(y, vz, s=2, c='k')
+            axs[4, 1].scatter(y, vz, s=2, c=colors)
             axs[4, 1].set(xlabel='y', ylabel='vz')
             axs[4, 1].set_xlim(-2, 2)
             axs[4, 1].set_ylim(-400, 400)
             
             #third column
             
-            axs[2, 2].scatter(z, vx, s=2, c='k')
+            axs[2, 2].scatter(z, vx, s=2, c=colors)
             axs[2, 2].set(xlabel='z', ylabel='vx')
             axs[2, 2].set_xlim(-2, 2)
             axs[2, 2].set_ylim(-400, 400)
             
-            axs[3, 2].scatter(z, vy, s=2, c='k')
+            axs[3, 2].scatter(z, vy, s=2, c=colors)
             axs[3, 2].set(xlabel='z', ylabel='vy')
             axs[3, 2].set_xlim(-2, 2)
             axs[3, 2].set_ylim(-400, 400)
             
-            axs[4, 2].scatter(z, vz, s=2, c='k')
+            axs[4, 2].scatter(z, vz, s=2, c=colors)
             axs[4, 2].set(xlabel='z', ylabel='vz')
             axs[4, 2].set_xlim(-2, 2)
             axs[4, 2].set_ylim(-400, 400)
             
             #fourth column
             
-            axs[3, 3].scatter(vx, vy, s=2, c='k')
+            axs[3, 3].scatter(vx, vy, s=2, c=colors)
             axs[3, 3].set(xlabel='vx', ylabel='vy')
             axs[3, 3].set_xlim(-400, 400)
             axs[3, 3].set_ylim(-400, 400)
             
-            axs[4, 3].scatter(vx, vz, s=2, c='k')
+            axs[4, 3].scatter(vx, vz, s=2, c=colors)
             axs[4, 3].set(xlabel='vx', ylabel='vz')
             axs[4, 3].set_xlim(-400, 400)
             axs[4, 3].set_ylim(-400, 400)
             
             #fifth column
             
-            axs[4, 4].scatter(vy, vz, s=2, c='k')
+            axs[4, 4].scatter(vy, vz, s=2, c=colors)
             axs[4, 4].set(xlabel='vy', ylabel='vz')
             axs[4, 4].set_xlim(-400, 400)
             axs[4, 4].set_ylim(-400, 400)
@@ -123,7 +124,7 @@ def maps(code_name, orbiter_name, Norbiters):
             plt.close()
         
         plt.figure()
-        plt.scatter(x, y, s=1, c='k')
+        plt.scatter(x, y, s=1, c=colors)
         plt.xlim(-2.0, 2.0)
         plt.ylim(-2.0, 2.0)
         plt.xlabel('x (kpc)', fontsize=12)

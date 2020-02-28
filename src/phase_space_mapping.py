1ea897fa67cc908d791d7b9422655534b8da4d39
Brian Cook <btcook@umich.edu>
2020-02-28 08:46:41 +0100

    only makes two snapshots of phase space map

--- src/phase_space_mapping.py
+++ src/phase_space_mapping.py
@@ -16,106 +16,108 @@ def maps(code_name, orbiter_name, Norbiters):
 
     for i, t in enumerate(sim_times):
     
-        w_all = pd.DataFrame(phase_space_data[i,:,:], 
-                             columns=['mass', 'x', 'y', 'z', 'vx', 'vy', 'vz'])
+        if i == 0 or i == len(sim_times)-1:
         
-        x,y,z = w_all['x'].tolist(), w_all['y'].tolist(), w_all['z'].tolist()
-        vx,vy,vz = w_all['vx'].tolist(), w_all['vy'].tolist(), w_all['vz'].tolist()
-
-        fig, axs = plt.subplots(5, 5)
-        
-        #first column
-        
-        axs[0, 0].scatter(x, y, s=2, c='k')
-        axs[0, 0].set(xlabel='x', ylabel='y')
-        axs[0, 0].set_xlim(-4, 4)
-        axs[0, 0].set_ylim(-4, 4)
-        
-        axs[1, 0].scatter(x, z, s=2, c='k')
-        axs[1, 0].set(xlabel='x', ylabel='z')
-        axs[1, 0].set_xlim(-4, 4)
-        axs[1, 0].set_ylim(-4, 4)
-        
-        axs[2, 0].scatter(x, vx,s=2, c='k')
-        axs[2, 0].set(xlabel='x', ylabel='vx')
-        axs[2, 0].set_xlim(-4, 4)
-        axs[2, 0].set_ylim(-400, 400)
-        
-        axs[3, 0].scatter(x, vy, s=2, c='k')
-        axs[3, 0].set(xlabel='z', ylabel='vy')
-        axs[3, 0].set_xlim(-4, 4)
-        axs[3, 0].set_ylim(-400, 400)
-        
-        axs[4, 0].scatter(x, vz, s=2, c='k')
-        axs[4, 0].set(xlabel='x', ylabel='vz')
-        axs[4, 0].set_xlim(-4, 4)
-        axs[4, 0].set_ylim(-400, 400)
-        
-        #second column
-        
-        axs[1, 1].scatter(y, z, s=2, c='k')
-        axs[1, 1].set(xlabel='y', ylabel='z')
-        axs[1, 1].set_xlim(-4, 4)
-        axs[1, 1].set_ylim(-4, 4)
-        
-        axs[2, 1].scatter(y, vx, s=2, c='k')
-        axs[2, 1].set(xlabel='y', ylabel='vx')
-        axs[2, 1].set_xlim(-4, 4)
-        axs[2, 1].set_ylim(-400, 400)
-        
-        axs[3, 1].scatter(y, vy, s=2, c='k')
-        axs[3, 1].set(xlabel='y', ylabel='vy')
-        axs[3, 1].set_xlim(-4, 4)
-        axs[3, 1].set_ylim(-400, 400)
-        
-        axs[4, 1].scatter(y, vz, s=2, c='k')
-        axs[4, 1].set(xlabel='y', ylabel='vz')
-        axs[4, 1].set_xlim(-4, 4)
-        axs[4, 1].set_ylim(-400, 400)
-        
-        #third column
-        
-        axs[2, 2].scatter(z, vx, s=2, c='k')
-        axs[2, 2].set(xlabel='z', ylabel='vx')
-        axs[2, 2].set_xlim(-4, 4)
-        axs[2, 2].set_ylim(-400, 400)
-        
-        axs[3, 2].scatter(z, vy, s=2, c='k')
-        axs[3, 2].set(xlabel='z', ylabel='vy')
-        axs[3, 2].set_xlim(-4, 4)
-        axs[3, 2].set_ylim(-400, 400)
-        
-        axs[4, 2].scatter(z, vz, s=2, c='k')
-        axs[4, 2].set(xlabel='z', ylabel='vz')
-        axs[4, 2].set_xlim(-4, 4)
-        axs[4, 2].set_ylim(-400, 400)
-        
-        #fourth column
-        
-        axs[3, 3].scatter(vx, vy, s=2, c='k')
-        axs[3, 3].set(xlabel='vx', ylabel='vy')
-        axs[3, 3].set_xlim(-400, 400)
-        axs[3, 3].set_ylim(-400, 400)
-        
-        axs[4, 3].scatter(vx, vz, s=2, c='k')
-        axs[4, 3].set(xlabel='vx', ylabel='vz')
-        axs[4, 3].set_xlim(-400, 400)
-        axs[4, 3].set_ylim(-400, 400)
-        
-        #fifth column
-        
-        axs[4, 4].scatter(vy, vz, s=2, c='k')
-        axs[4, 4].set(xlabel='vy', ylabel='vz')
-        axs[4, 4].set_xlim(-400, 400)
-        axs[4, 4].set_ylim(-400, 400)
-        
-        # Hide x labels and tick labels for top plots and y ticks for right plots.
-        for ax in axs.flat:
-            ax.label_outer()
+            w_all = pd.DataFrame(phase_space_data[i,:,:], 
+                                 columns=['mass', 'x', 'y', 'z', 'vx', 'vy', 'vz'])
             
-        fig.suptitle('Time = %.02f Myr'%(t), fontsize=14)
-        plt.savefig('phase_space_map_frame=%s_%s_%s.png'%(str(i).rjust(4, '0'), code_name, orbiter_name))
-        plt.close()
+            x,y,z = w_all['x'].tolist(), w_all['y'].tolist(), w_all['z'].tolist()
+            vx,vy,vz = w_all['vx'].tolist(), w_all['vy'].tolist(), w_all['vz'].tolist()
+    
+            fig, axs = plt.subplots(5, 5)
+            
+            #first column
+            
+            axs[0, 0].scatter(x, y, s=2, c='k')
+            axs[0, 0].set(xlabel='x', ylabel='y')
+            axs[0, 0].set_xlim(-4, 4)
+            axs[0, 0].set_ylim(-4, 4)
+            
+            axs[1, 0].scatter(x, z, s=2, c='k')
+            axs[1, 0].set(xlabel='x', ylabel='z')
+            axs[1, 0].set_xlim(-4, 4)
+            axs[1, 0].set_ylim(-4, 4)
+            
+            axs[2, 0].scatter(x, vx,s=2, c='k')
+            axs[2, 0].set(xlabel='x', ylabel='vx')
+            axs[2, 0].set_xlim(-4, 4)
+            axs[2, 0].set_ylim(-400, 400)
+            
+            axs[3, 0].scatter(x, vy, s=2, c='k')
+            axs[3, 0].set(xlabel='z', ylabel='vy')
+            axs[3, 0].set_xlim(-4, 4)
+            axs[3, 0].set_ylim(-400, 400)
+            
+            axs[4, 0].scatter(x, vz, s=2, c='k')
+            axs[4, 0].set(xlabel='x', ylabel='vz')
+            axs[4, 0].set_xlim(-4, 4)
+            axs[4, 0].set_ylim(-400, 400)
+            
+            #second column
+            
+            axs[1, 1].scatter(y, z, s=2, c='k')
+            axs[1, 1].set(xlabel='y', ylabel='z')
+            axs[1, 1].set_xlim(-4, 4)
+            axs[1, 1].set_ylim(-4, 4)
+            
+            axs[2, 1].scatter(y, vx, s=2, c='k')
+            axs[2, 1].set(xlabel='y', ylabel='vx')
+            axs[2, 1].set_xlim(-4, 4)
+            axs[2, 1].set_ylim(-400, 400)
+            
+            axs[3, 1].scatter(y, vy, s=2, c='k')
+            axs[3, 1].set(xlabel='y', ylabel='vy')
+            axs[3, 1].set_xlim(-4, 4)
+            axs[3, 1].set_ylim(-400, 400)
+            
+            axs[4, 1].scatter(y, vz, s=2, c='k')
+            axs[4, 1].set(xlabel='y', ylabel='vz')
+            axs[4, 1].set_xlim(-4, 4)
+            axs[4, 1].set_ylim(-400, 400)
+            
+            #third column
+            
+            axs[2, 2].scatter(z, vx, s=2, c='k')
+            axs[2, 2].set(xlabel='z', ylabel='vx')
+            axs[2, 2].set_xlim(-4, 4)
+            axs[2, 2].set_ylim(-400, 400)
+            
+            axs[3, 2].scatter(z, vy, s=2, c='k')
+            axs[3, 2].set(xlabel='z', ylabel='vy')
+            axs[3, 2].set_xlim(-4, 4)
+            axs[3, 2].set_ylim(-400, 400)
+            
+            axs[4, 2].scatter(z, vz, s=2, c='k')
+            axs[4, 2].set(xlabel='z', ylabel='vz')
+            axs[4, 2].set_xlim(-4, 4)
+            axs[4, 2].set_ylim(-400, 400)
+            
+            #fourth column
+            
+            axs[3, 3].scatter(vx, vy, s=2, c='k')
+            axs[3, 3].set(xlabel='vx', ylabel='vy')
+            axs[3, 3].set_xlim(-400, 400)
+            axs[3, 3].set_ylim(-400, 400)
+            
+            axs[4, 3].scatter(vx, vz, s=2, c='k')
+            axs[4, 3].set(xlabel='vx', ylabel='vz')
+            axs[4, 3].set_xlim(-400, 400)
+            axs[4, 3].set_ylim(-400, 400)
+            
+            #fifth column
+            
+            axs[4, 4].scatter(vy, vz, s=2, c='k')
+            axs[4, 4].set(xlabel='vy', ylabel='vz')
+            axs[4, 4].set_xlim(-400, 400)
+            axs[4, 4].set_ylim(-400, 400)
+            
+            # Hide x labels and tick labels for top plots and y ticks for right plots.
+            for ax in axs.flat:
+                ax.label_outer()
+                
+            fig.suptitle('Time = %.02f Myr'%(t), fontsize=14)
+            plt.savefig('phase_space_map_frame=%s_%s_%s.png'%(str(i).rjust(4, '0'), code_name, orbiter_name))
+            plt.close()
         
         plt.figure()
         plt.scatter(x, y, s=1, c='k')

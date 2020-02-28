db5bcd74229e465d76fc264c15f54920daaf650d
Brian <btcook@umich.edu>
2020-02-28 14:49:17 +0100

    name snapshots by Norbiters

--- src/FebFive.sh
+++ src/FebFive.sh
@@ -9,7 +9,7 @@ convert -delay 2 'snapshot_tree_SingleStar_*.png' -loop 0 tree_singlestar.gif
 convert -delay 2 'snapshot_Nbody_SingleCluster_*.png' -loop 0 Nbody_singlecluster.gif
 convert -delay 2 'snapshot_tree_SingleCluster_*.png' -loop 0 tree_singlecluster.gif
 
-rm -rf snapshot_*.png
+#rm -rf snapshot_*.png
 
 mv for_enbid_*.txt ../data/enbid_files
 mv times_*.txt ../data/simulation_times
--- src/testing_nemesis.py
+++ src/testing_nemesis.py
@@ -59,7 +59,7 @@ if __name__ in '__main__':
     
     masses = np.loadtxt(data_directory+'ICs/cluster_masses_for_sampling.txt')
 
-    Norbiters_list = [ 1, 2 ] #need to make into a list at some point
+    Norbiters_list = [ 1 ] #need to make into a list at some point
     orbiter_names = [ 'SingleStar', 'SingleCluster' ]
     code_names = ['tree', 'Nbody' ]#, 'nemesis'
 

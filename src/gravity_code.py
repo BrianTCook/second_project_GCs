98949bed8d9123f2abf354a021ce908cbec5a0a5
Brian Cook <btcook@umich.edu>
2020-03-02 14:01:40 +0100

    adding particles from HierarchicalParticles class

--- src/gravity_code.py
+++ src/gravity_code.py
@@ -80,6 +80,8 @@ def gravity_code_setup(code_name, orbiter_name, Mgalaxy, Rgalaxy, galaxy_code, s
         need add_subsystem and assign_subsystem in HierarchicalParticles I think
         '''
         
+        parts=HierarchicalParticles(all_boodies)
+        
         dt=smaller_nbody_power_of_two(0.1 | units.Myr, converter_parent)
                 
         nemesis=Nemesis( parent_worker, sub_worker, py_worker)

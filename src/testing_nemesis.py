024c886105f57d15c24410d077de6c38466f284f
Brian Cook <btcook@umich.edu>
2020-03-09 22:55:28 +0100

    run for 32 clusters, fingers crossed it works

--- src/testing_nemesis.py
+++ src/testing_nemesis.py
@@ -60,7 +60,7 @@ if __name__ in '__main__':
     masses_all = np.loadtxt(data_directory+'ICs/cluster_masses_for_sampling.txt')
 
     #logN_max = 5
-    Norbiters_list = [ 16 ] #2**i for i in range(logN_max)
+    Norbiters_list = [ 32 ] #2**i for i in range(logN_max)
     orbiter_names = [ 'SingleCluster' ] #,, 'SingleStar',  'BinaryCluster' 
     code_names = [ 'tree' ] #, 'Nbody', 'nemesis' 
 
@@ -90,12 +90,10 @@ if __name__ in '__main__':
                 print('\\\\\\\\\\\\\\\\\\\\\\\\')
                 
                 #t_init = time.time()
-                
-                '''
+
                 simulation(code_name, orbiter_name, potential, Mgalaxy, Rgalaxy, 
                            sepBinary, rvals, phivals, zvals, vrvals, vphivals, vzvals, 
                            masses, Norbiters, tend, dt)
-                '''
                 
                 #t_final = time.time()
                 

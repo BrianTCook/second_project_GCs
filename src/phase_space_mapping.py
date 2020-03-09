3603c4cad21285952e0b2f93874aebc664993846
Brian Cook <btcook@umich.edu>
2020-03-09 20:35:34 +0100

    no padding

--- src/phase_space_mapping.py
+++ src/phase_space_mapping.py
@@ -182,7 +182,7 @@ def maps(code_name, orbiter_name, Norbiters):
                         for ax in axs.flat:
                             ax.label_outer()
                             
-                        fig.suptitle('Time = %.02f Myr'%(t), fontsize=14, pad=5)
+                        fig.suptitle('Time = %.02f Myr'%(t), fontsize=14)
                         plt.subplots_adjust(wspace=0, hspace=0)
                         plt.savefig('phase_space_map_frame_%s_%s_%s_Norbiters_%s.pdf'%(str(i).rjust(5, '0'), code_name, orbiter_name, str(Norbiters)))
                         plt.close()

f21a49f4cbca320fc08cf78fefbac8c421ddcd6d
Brian Cook <btcook@umich.edu>
2020-03-09 19:24:30 +0100

    only do tree code

--- src/testing_nemesis.py
+++ src/testing_nemesis.py
@@ -61,7 +61,7 @@ if __name__ in '__main__':
     #logN_max = 5
     Norbiters_list = [ 128 ] #2**i for i in range(logN_max)
     orbiter_names = [ 'SingleCluster' ] #,, 'SingleStar',  'BinaryCluster' 
-    code_names = [ 'tree', 'Nbody', 'nemesis' ]
+    code_names = [ 'tree' ] #, 'Nbody', 'nemesis' 
 
     t0 = time.time()
     

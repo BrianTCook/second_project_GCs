949de4467566907751de523075177f1e01451617
Brian Cook <btcook@umich.edu>
2020-03-25 11:21:38 -0400

    how many cells for 6D grid?

--- src/fw_entropy.py
+++ src/fw_entropy.py
@@ -7,26 +7,22 @@ Created on Thu Mar  5 13:02:16 2020
 """
 
 import numpy as np
+import glob
+import time
+
 from scipy.interpolate import griddata
 
-def get_6D_fw(points, values):
-
-    points = np.load()
-    values = np.loadtxt()
+def get_6D_fw(points, values, N):
     
-    x = np.linspace(-4., 4., 100)
-    y = np.linspace(-4., 4., 100)
-    z = np.linspace(-4., 4., 100)
+    spatial = list(np.linspace(-4., 4., N))  
+    velocity = list(np.linspace(-500., 500., N))
     
-    vx = np.linspace(-500., 500., 100)
-    vy = np.linspace(-500., 500., 100)
-    vz = np.linspace(-500., 500., 100)
-    
-    X, Y, Z, VX, VY, VZ = np.meshgrid(x,y, z, vx, vy, vz)
+    X, Y, Z, VX, VY, VZ = np.meshgrid(*(spatial, spatial, spatial, 
+                                        velocity, velocity, velocity))
 
     #Ti is 6-D interpolation using method=method
     Ti = griddata(points, values, (X, Y, Z, VX, VY, VZ), method='linear')
-        
+    
     return Ti
 
 def simpson(xvals, fvals): #Simpson's integration rule, \int_{a}^{b} f(x) dx with N sample points
@@ -54,40 +50,61 @@ def get_entropy(points, values):
     the value within it will be the information entropy
     '''
     
-    N = 100
+    N = 5
     
-    spatial_vals = np.linspace(-2, 2, N)
-    velocity_vals = np.linspace(-500, 500, N)
+    spatial_vals = np.linspace(-4., 4., N)
+    velocity_vals = np.linspace(-500., 500., N)
     
-    grid = get_6D_fw(points, values)
+    grid = get_6D_fw(points, values, N)
     
     sixD_arr = np.multiply(grid, np.log(grid))
+    sixD_arr = np.nan_to_num(sixD_arr) # 0 * np.log(0) is nan
     
     fiveD_arr = [ simpson(velocity_vals, sixD_arr[i,j,k,l,m,:])
                   for i in range(N) for j in range(N) 
                   for k in range(N) for l in range(N) 
                   for m in range(N) ]
     
-    fiveD_arr = fiveD_arr.reshape(N, N, N, N, N)
+    fiveD_arr = np.asarray(fiveD_arr).reshape(N, N, N, N, N)
     
     fourD_arr = [ simpson(velocity_vals, fiveD_arr[i,j,k,l,:])
                   for i in range(N) for j in range(N) 
                   for k in range(N) for l in range(N) ]
     
-    fourD_arr = fourD_arr.reshape(N, N, N, N)
+    fourD_arr = np.asarray(fourD_arr).reshape(N, N, N, N)
     
     threeD_arr = [ simpson(velocity_vals, fourD_arr[i,j,k,:])
                    for i in range(N) for j in range(N) 
                    for k in range(N) ]
     
-    threeD_arr = threeD_arr.reshape(N, N, N)
+    threeD_arr = np.asarray(threeD_arr).reshape(N, N, N)
     
     twoD_arr = [ simpson(spatial_vals, threeD_arr[i,j,:])
                  for i in range(N) for j in range(N)  ]
     
-    twoD_arr = fiveD_arr.reshape(N, N)
+    twoD_arr = np.asarray(twoD_arr).reshape(N, N)
     
     oneD_arr = [ simpson(spatial_vals, twoD_arr[i,:])
               for i in range(N)  ]
     
     return simpson(spatial_vals, oneD_arr)
+
+if __name__ in '__main__':
+    
+    point_files = glob.glob('/Users/BrianTCook/Desktop/Thesis/second_project_gcs/data/enbid_files/*1.ascii')
+    
+    t0 = time.time()
+    
+    for point_file in point_files:
+        
+        points = np.loadtxt(point_file)[:, 1:]
+        values = np.random.rand(points.shape[0])
+        
+        S = get_entropy(points, values)
+        
+        tf = time.time()
+        
+        print('')
+        print('computation time: %.04f seconds'%(tf-t0))
+        print('S = %.04f'%(S))
+        

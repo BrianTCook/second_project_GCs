ae3a04eaac508bd3209ec99fa2339c24f45d68a9
BrianTCook <btcook@umich.edu>
2020-04-04 18:12:31 +0200

    need to get enbid pipeline working today

--- alice_bash/setup.sh
+++ alice_bash/setup.sh
@@ -1,3 +1,3 @@
 module load Python/3.6.4-foss-2018a
 module load AMUSE/13.1.0-foss-2018a-Python-3.6.4
-module load /home/s1780638/amuse_env/site_packages/galpy/
+module load home/s1780638/amuse_env/site-packages/galpy

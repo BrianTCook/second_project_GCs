b6b525df6c5e030276c77dfe50f2f5b6985d547f
Brian Cook <btcook@umich.edu>
2020-04-08 23:43:48 -0400

    computes entropy separately, encouraging result!

--- src/fw_entropy.py
+++ src/fw_entropy.py
@@ -78,79 +78,8 @@ def nonuniform_bins(list_of_values, Nmax_in_bin):
     centers = [ 0.5*(edges[i]+edges[i+1]) for i in range(len(edges)-1)  ]
     
     return centers
-'''
-
-def lattices_maker(points, uniformity):
     
-    '''
-    takes 6D phase space coordinates
-    tessellates phase space s.t. there are < Npoints_max
-    '''
-    
-    cluster_populations = np.loadtxt('/home/brian/Desktop/second_project_gcs/data/Nstars_in_clusters.txt')
-
-    for cluster in cluster_populations
-    
-    xvals, yvals, zvals = points[:,0], points[:,1], points[:,2]
-    vxvals, vyvals, vzvals = points[:,3], points[:,4], points[:,5]
-    
-    if uniformity == 'uniform':
-    
-        Ncells = 12
-        
-        x_spatial = np.linspace(min(xvals), max(xvals), Ncells)
-        y_spatial = np.linspace(min(yvals), max(yvals), Ncells)
-        z_spatial = np.linspace(min(zvals), max(zvals), Ncells)
-        x_velocity = np.linspace(min(vxvals), max(vxvals), Ncells)
-        y_velocity = np.linspace(min(vyvals), max(vyvals), Ncells)
-        z_velocity = np.linspace(min(vzvals), max(vzvals), Ncells)
-        
-    if uniformity == 'non-uniform':
-    
-        Nmax_in_bin = 50
-        
-        x_spatial = nonuniform_bins(xvals, Nmax_in_bin)
-        y_spatial = nonuniform_bins(yvals, Nmax_in_bin)
-        z_spatial = nonuniform_bins(zvals, Nmax_in_bin) 
-        x_velocity = nonuniform_bins(vxvals, Nmax_in_bin)
-        y_velocity = nonuniform_bins(vyvals, Nmax_in_bin)
-        z_velocity = nonuniform_bins(vzvals, Nmax_in_bin) 
-    
-    #need to make non-uniform samples so that each cell has <= Nmax_in_bin sample points in it
-
-    return x_spatial, y_spatial, z_spatial, x_velocity, y_velocity, z_velocity
-
-def get_6D_fw(points, values, uniformity):
-    
-    print('unique fw values: ', np.unique(values).shape)
-
-    X, Y, Z, VX, VY, VZ = np.meshgrid(*lattice_maker(points, uniformity))
-
-    #Ti is 6-D interpolation using method=method    
-    grid_z0 = griddata(points, values, (X, Y, Z, VX, VY, VZ), method='nearest')    
-
-    print('unique elements in interpolated lattice: ', np.unique(grid_z0).shape)
-    
-    return grid_z0
-
-def simpson(xvals, fvals): #Simpson's integration rule, \int_{a}^{b} f(x) dx with N sample points
-
-    N_simp = len(fvals)
-    h = xvals[1]-xvals[0]
-
-    a, b = 0, len(fvals)-1
-
-    I = fvals[a]+fvals[b]
-
-    odds = [ 4*fvals[a + k] for k in range(1,N_simp,2) ]
-    evens = [ 2*fvals[a + k] for k in range(2,N_simp,2) ]
-    I += sum(odds) + sum(evens)
-
-    I *= h/3.
-    
-    return I
-
-def simpson_nonuniform(x, f):
+    def simpson_nonuniform(x, f):
     
     """
     FOUND ON WIKIPEDIA
@@ -195,55 +124,19 @@ def simpson_nonuniform(x, f):
         result -= f[N - 2] * h[N - 1]**3\
                      / ( 6 * h[N - 2] * ( h[N - 2] + h[N - 1] ) )
     return result
-        
-
-def get_entropy(points, values, uniformity):
     
-    '''
-    right now have N x N x N x N x N x N array
-    need to integrate s.t. we get a 1 x 1 x 1 x 1 x 1 x 1 array
-    the value within it will be the information entropy
-    '''
+    if uniformity == 'non-uniform':
     
-    x_spatial, y_spatial, z_spatial, x_velocity, y_velocity, z_velocity = lattice_maker(points, uniformity)
-    grid = get_6D_fw(points, values, uniformity)
+        Nmax_in_bin = 50
+        
+        x_spatial = nonuniform_bins(xvals, Nmax_in_bin)
+        y_spatial = nonuniform_bins(yvals, Nmax_in_bin)
+        z_spatial = nonuniform_bins(zvals, Nmax_in_bin) 
+        x_velocity = nonuniform_bins(vxvals, Nmax_in_bin)
+        y_velocity = nonuniform_bins(vyvals, Nmax_in_bin)
+        z_velocity = nonuniform_bins(vzvals, Nmax_in_bin) 
     
-    Nx, Ny, Nz = len(x_spatial), len(y_spatial), len(z_spatial)
-    Nvx, Nvy, Nvz = len(x_velocity), len(y_velocity), len(z_velocity)
-    
-    sixD_arr = np.multiply(grid, np.log(grid))
-    
-    if uniformity == 'uniform':
-        
-        fiveD_arr = [ simpson(z_velocity, sixD_arr[i,j,k,l,m,:])
-                      for i in range(Nx) for j in range(Ny) 
-                      for k in range(Nz) for l in range(Nvx) 
-                      for m in range(Nvy) ]
-        
-        fiveD_arr = np.asarray(fiveD_arr).reshape(Nx, Ny, Nz, Nvx, Nvy)
-         
-        fourD_arr = [ simpson(y_velocity, fiveD_arr[i,j,k,l,:])
-                      for i in range(Nx) for j in range(Ny) 
-                      for k in range(Nz) for l in range(Nvx)  ]
-        
-        fourD_arr = np.asarray(fourD_arr).reshape(Nx, Ny, Nz, Nvx)
-        
-        threeD_arr = [ simpson(x_velocity, fourD_arr[i,j,k,:])
-                       for i in range(Nx) for j in range(Ny) 
-                       for k in range(Nz) ]
-        
-        threeD_arr = np.asarray(threeD_arr).reshape(Nx, Ny, Nz)
-        
-        twoD_arr = [ simpson(z_spatial, threeD_arr[i,j,:])
-                     for i in range(Nx) for j in range(Ny) ]
-        
-        twoD_arr = np.asarray(twoD_arr).reshape(Nx, Ny)
-        
-        oneD_arr = [ simpson(y_spatial, twoD_arr[i,:])
-                  for i in range(Nx)  ]
-        
-        #returns entropy
-        return simpson(x_spatial, oneD_arr)
+    #need to make non-uniform samples so that each cell has <= Nmax_in_bin sample points in it
     
     if uniformity == 'non-uniform':
     
@@ -276,6 +169,149 @@ def get_entropy(points, values, uniformity):
         
         #returns entropy
         return simpson_nonuniform(x_spatial, oneD_arr)
+    
+'''
+
+
+def lattices_maker(points, uniformity, Norbiters):
+    
+    '''
+    takes 6D phase space coordinates
+    tessellates phase space s.t. there are < Npoints_max
+    '''
+    
+    datadir = '/Users/BrianTCook/Desktop/Thesis/second_project_GCs/data/'
+    cluster_populations = np.loadtxt(datadir + 'Nstars_in_clusters.txt')
+    cluster_populations = list(cluster_populations[:Norbiters])
+    grids = []
+
+    for k, number_of_stars in enumerate(cluster_populations):
+        
+        starting_index = int(np.sum( cluster_populations[:k] ))
+        ending_index = starting_index + int(number_of_stars)
+    
+        xvals, yvals, zvals = points[starting_index:ending_index,0], points[starting_index:ending_index,1], points[starting_index:ending_index,2]
+        vxvals, vyvals, vzvals = points[starting_index:ending_index,3], points[starting_index:ending_index,4], points[starting_index:ending_index,5]
+        
+        if uniformity == 'uniform':
+        
+            Ncells = 10
+            
+            x_spatial = np.linspace(min(xvals), max(xvals), Ncells)
+            y_spatial = np.linspace(min(yvals), max(yvals), Ncells)
+            z_spatial = np.linspace(min(zvals), max(zvals), Ncells)
+            x_velocity = np.linspace(min(vxvals), max(vxvals), Ncells)
+            y_velocity = np.linspace(min(vyvals), max(vyvals), Ncells)
+            z_velocity = np.linspace(min(vzvals), max(vzvals), Ncells)
+        
+        grids.append([x_spatial, y_spatial, z_spatial, x_velocity, y_velocity, z_velocity])
+
+    return grids
+
+def get_6D_fw(points, values, uniformity, Norbiters):
+    
+    datadir = '/Users/BrianTCook/Desktop/Thesis/second_project_GCs/data/'
+    cluster_populations = np.loadtxt(datadir + 'Nstars_in_clusters.txt')
+    cluster_populations = list(cluster_populations[:Norbiters])
+    grids = lattices_maker(points, uniformity, Norbiters)
+    fw_values_interpolated = []
+
+    for k, number_of_stars in enumerate(cluster_populations):
+
+        starting_index = int(np.sum( cluster_populations[:k] ))
+        ending_index = starting_index + int(number_of_stars)
+        
+        print('unique fw values: ', np.unique(values[starting_index:ending_index]).shape)
+    
+        X, Y, Z, VX, VY, VZ = np.meshgrid(*grids[k])
+    
+        #Ti is 6-D interpolation using method=method    
+        grid_z0 = griddata(points[starting_index:ending_index,:], values[starting_index:ending_index], (X, Y, Z, VX, VY, VZ), method='nearest')    
+    
+        print('unique elements in interpolated lattice: ', np.unique(grid_z0).shape)
+        
+        fw_values_interpolated.append(grid_z0)
+        
+    return fw_values_interpolated
+
+def simpson(xvals, fvals): #Simpson's integration rule, \int_{a}^{b} f(x) dx with N sample points
+
+    N_simp = len(fvals)
+    h = xvals[1]-xvals[0]
+
+    a, b = 0, len(fvals)-1
+
+    I = fvals[a]+fvals[b]
+
+    odds = [ 4*fvals[a + k] for k in range(1,N_simp,2) ]
+    evens = [ 2*fvals[a + k] for k in range(2,N_simp,2) ]
+    I += sum(odds) + sum(evens)
+
+    I *= h/3.
+    
+    return I
+        
+
+def get_entropy(points, values, uniformity, Norbiters):
+    
+    '''
+    right now have N x N x N x N x N x N array
+    need to integrate s.t. we get a 1 x 1 x 1 x 1 x 1 x 1 array
+    the value within it will be the information entropy
+    '''
+    
+    datadir = '/Users/BrianTCook/Desktop/Thesis/second_project_GCs/data/'
+    cluster_populations = np.loadtxt(datadir + 'Nstars_in_clusters.txt')
+    cluster_populations = list(cluster_populations[:Norbiters])
+    
+    fw_values_interpolated = get_6D_fw(points, values, uniformity, Norbiters)
+    grids = lattices_maker(points, uniformity, Norbiters)
+
+    S = 0 #entropy
+
+    for k, number_of_stars in enumerate(cluster_populations):
+    
+        x_spatial, y_spatial, z_spatial, x_velocity, y_velocity, z_velocity = grids[k]
+        fw_interpolated = fw_values_interpolated[k]
+    
+        Nx, Ny, Nz = len(x_spatial), len(y_spatial), len(z_spatial)
+        Nvx, Nvy, Nvz = len(x_velocity), len(y_velocity), len(z_velocity)
+        
+        sixD_arr = np.multiply(fw_interpolated, np.log(fw_interpolated))
+        
+        if uniformity == 'uniform':
+            
+            fiveD_arr = [ simpson(z_velocity, sixD_arr[i,j,k,l,m,:])
+                          for i in range(Nx) for j in range(Ny) 
+                          for k in range(Nz) for l in range(Nvx) 
+                          for m in range(Nvy) ]
+            
+            fiveD_arr = np.asarray(fiveD_arr).reshape(Nx, Ny, Nz, Nvx, Nvy)
+             
+            fourD_arr = [ simpson(y_velocity, fiveD_arr[i,j,k,l,:])
+                          for i in range(Nx) for j in range(Ny) 
+                          for k in range(Nz) for l in range(Nvx)  ]
+            
+            fourD_arr = np.asarray(fourD_arr).reshape(Nx, Ny, Nz, Nvx)
+            
+            threeD_arr = [ simpson(x_velocity, fourD_arr[i,j,k,:])
+                           for i in range(Nx) for j in range(Ny) 
+                           for k in range(Nz) ]
+            
+            threeD_arr = np.asarray(threeD_arr).reshape(Nx, Ny, Nz)
+            
+            twoD_arr = [ simpson(z_spatial, threeD_arr[i,j,:])
+                         for i in range(Nx) for j in range(Ny) ]
+            
+            twoD_arr = np.asarray(twoD_arr).reshape(Nx, Ny)
+            
+            oneD_arr = [ simpson(y_spatial, twoD_arr[i,:])
+                      for i in range(Nx)  ]
+            
+            S += simpson(x_spatial, oneD_arr)
+        
+    #returns entropy
+    return S
 
 if __name__ in '__main__':
     
@@ -283,7 +319,7 @@ if __name__ in '__main__':
     
     t0 = time.time()
     
-    logN_max = 0
+    logN_max = 6
     
     xvals_all, yvals_all = [ [] for i in range(logN_max+1) ], [ [] for i in range(logN_max+1) ]
     
@@ -311,7 +347,7 @@ if __name__ in '__main__':
             values = np.loadtxt(point_file + '_output.est')
             
             uniformity = 'uniform'
-            S = get_entropy(points, values, uniformity)
+            S = get_entropy(points, values, uniformity, Nclusters)
             #fw_map, x0, x1, y0, y1 = get_map(points, values)
             #print('number of unique elements in fw_map: ', np.unique(fw_map).shape)
             
@@ -333,7 +369,7 @@ if __name__ in '__main__':
     for logN in range(logN_max+1):
         
         xvals, yvals = xvals_all[logN], yvals_all[logN]
-        plt.scatter(xvals, yvals, label=r'$\log_{2} N_{\mathrm{clusters}}$ = %i'%(logN), s=4)
+        plt.plot(xvals, yvals, label=r'$\log_{2} N_{\mathrm{clusters}}$ = %i'%(logN), linewidth=1)
     
     plt.gca().set_yscale('log')
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=12)

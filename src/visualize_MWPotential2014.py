f65ae347771c34fd20faf5b3004a572f54a55e2a
Brian <btcook@umich.edu>
2020-04-20 23:26:46 +0200

    have a MWPotential2014 plot

--- src/visualize_MWPotential2014.py
+++ src/visualize_MWPotential2014.py
@@ -7,6 +7,10 @@ Created on Mon Apr 20 16:52:16 2020
 """
 
 from galpy.potential import MWPotential2014, plotPotentials
+import matplotlib.pyplot as plt
 
-plotPotentials(MWPotential2014, rmin=0.01, rmax=1.9, nrs=51, 
-               zmin=-0.99, zmax=0.99, nzs=51)
+ahh = plotPotentials(MWPotential2014, rmin=0.01, rmax=0.9, nrs=51, 
+               zmin=-0.49, zmax=0.49, nzs=51, justcontours=True, ncontours=21,
+	       aspect=None)
+
+plt.savefig('MWPotential2014_plot.pdf')

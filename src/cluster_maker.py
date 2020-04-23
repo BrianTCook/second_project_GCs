86d0a5ab2530b07fe81d042945efa4955a393e19
Brian Cook <btcook@umich.edu>
2020-04-23 17:53:02 -0400

    :|

--- src/cluster_maker.py
+++ src/cluster_maker.py
@@ -96,7 +96,7 @@ def star_cluster(rvals, phivals, zvals, vrvals, vphivals, vzvals, masses, index,
     vr_init, vphi_init, vz_init = vrvals[index], vphivals[index], vzvals[index]
     
     #data_directory = '/home/s1780638/second_project_gcs/data/'
-    data_directory = 'home/brian/Desktop/second_project_gcs/data/'
+    data_directory = '/home/brian/Desktop/second_project_gcs/data/'
     
     star_masses = np.loadtxt(data_directory+'star_masses/star_masses_index=%i.txt'%index)
     Mcluster = np.sum( star_masses )

0ae30e9251f33ee9c238e8669ef0e58a18518fbb
Brian Cook <btcook@umich.edu>
2020-04-23 17:51:59 -0400

    why does code have to be so picky

--- src/cluster_maker.py
+++ src/cluster_maker.py
@@ -122,7 +122,7 @@ def orbiter(code_name, orbiter_name, Mgalaxy, Rgalaxy, sepBinary,
     index_sorted = indices_dict[index]
     
     #data_directory = '/home/s1780638/second_project_gcs/data/'
-    data_directory = 'home/brian/Desktop/second_project_gcs/data/'
+    data_directory = '/home/brian/Desktop/second_project_gcs/data/'
     star_masses = np.loadtxt(data_directory+'star_masses/star_masses_index=%s.txt'%(str(index_sorted)))
     
     converter_parent = nbody_system.nbody_to_si(Mgalaxy, Rgalaxy)

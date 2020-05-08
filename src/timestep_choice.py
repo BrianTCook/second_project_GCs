053e1b87e987c7fee497da317878c7f5ec6800bf
Brian Cook <btcook@umich.edu>
2020-05-07 20:29:54 -0400

    have energy plot, now need simulation data!

--- src/timestep_choice.py
+++ src/timestep_choice.py
@@ -10,7 +10,7 @@ import numpy as np
 import glob
 import matplotlib.pyplot as plt
 
-dt_values = [ 0.001, 0.005, 0.01, 0.05, 0.1, 0.5 ]
+dt_values = [ 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5 ]
 tend = 100.
 
 energy_values = glob.glob('/Users/BrianTCook/Desktop/Thesis/second_project_gcs/data/tree_data/energy_check_data/*.txt')
@@ -25,15 +25,15 @@ for i, dt in enumerate(dt_values):
     
     energies = np.loadtxt(energy_values[i])  
     sim_times = np.linspace(0., tend+dt, len(energies))
-    plt.plot(sim_times, energies, linewidth=1, label=r'$\Delta t$ = %.02f Myr'%(dt))
+    plt.plot(sim_times, energies, linewidth=0.5, label=r'$\Delta t$ = %.03f Myr'%(dt))
     
-plt.ylim(-0.15, 0.05)
+#plt.ylim(-0.15, 0.05)
 plt.axhline(y=0, linewidth=1, linestyle='--', color='k')
 plt.xlabel(r'$t_{\mathrm{sim}}$ (Myr)', fontsize=16)
 plt.ylabel(r'$\Delta E / E(t=0)$', fontsize=16)
 plt.gca().tick_params(labelsize='large')
 plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=10)
-plt.annotate(r'$\log_{2}N_{\mathrm{clusters}}=3$', xy=(0.1,0.1), xycoords='axes fraction', fontsize=14)
+plt.annotate(r'$\log_{2}N_{\mathrm{clusters}}=0$', xy=(0.05,0.1), xycoords='axes fraction', fontsize=8)
 plt.title('Energy Conservation, Tree Code', fontsize=16)
 plt.tight_layout()
 plt.savefig('energy_conservation_plot.pdf')
